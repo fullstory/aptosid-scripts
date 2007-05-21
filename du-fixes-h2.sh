ce6ac844da5cacc23f88b4e7fbceae74b647bcef
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-05-21 21:28:23 +0000

    * import fixed du-fixes-h2.sh downloader stub. * minor coding style changes.

--- du-fixes-h2.sh
+++ du-fixes-h2.sh
@@ -1,32 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 ## note: save file as du-fixes-h2.sh
 
 PREFIX="/usr/local/bin/"
-UPSTREAM="http://techpatterns.com/"
+UPSTREAM="http://techpatterns.com/downloads/distro/"
 DUF="du-fixes-h2.sh"
 
-if [ "$(id -u)" -ne 0 ]; then
-	[ -x /usr/bin/su-me ] && DISPLAY="" exec su-me "$0" "$@"
-	echo "Error: You must be root to run this script!"
+error_handler()
+{
+	local message=''
+	case $1 in
+		1)
+			message='You must be root to run this script!'
+			;;
+		2)
+			message='Failed to download script!'
+			;;
+		3)
+			message="${PREFIX}${DUF} not executable!"
+			;;
+		4)
+			message='Script download exited with errors.'
+			;;
+		*)
+			message='Unknown error, exiting now.'
+			;;
+	esac
+	echo "ERROR: $message - Error No: $1"
+	
+	exit $1
+}
 
-	exit 1
+# user login test cannot run first because it kills -h
+
+# if it exists, du-fixes will handle the root tests. Root tests cannot
+# be run first because they kill the -h help function
+if [ -x "${PREFIX}${DUF}" ]; then
+	exec "${PREFIX}${DUF}" "$@"
 else
+	# only in case of requirement to download script do root test, and just exit
+	# if not root, anything else is counterintuitive I think for users.
+	[ "$(id -u)" -ne 0 ] && error_handler 1
+
+	if [ ! -f "${PREFIX}${DUF}" ]; then
+		wget -Nc -O"${PREFIX}${DUF}" "${UPSTREAM}${DUF}" || error_handler 4
+	fi
+	[ -f "${PREFIX}${DUF}" ]   || error_handler 2
+	
+	[ ! -x "${PREFIX}${DUF}" ] || chmod +x "${PREFIX}${DUF}"
+	
 	if [ -x "${PREFIX}${DUF}" ]; then
-		exec "${PREFIX}${DUF}"
+		exec "${PREFIX}${DUF}" "$@"
 	else
-		[ -f "${PREFIX}${DUF}" ] ||	wget -Nc -O"${PREFIX}${DUF}" "${UPSTREAM}${DUF}"
-
-		if [ -f "${PREFIX}${DUF}" ]; then
-			echo "ERROR: Failed to download script!"
-			exit 1
-		fi
-
-		[ -x "${PREFIX}${DUF}" ] ||	chmod +x "${PREFIX}${DUF}"
-
-		[ -x "${PREFIX}${DUF}" ] &&	exec "${PREFIX}${DUF}"
-
-		echo "ERROR: ${PREFIX}${DUF} not executable!"
-		exit 2
+		error_handler 3
 	fi
 fi
 
