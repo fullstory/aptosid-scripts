e5daa26bbb8983c075b03a977bfabb92bac226b2
Kel Modderman <kel@otaku42.de>
2007-05-23 12:32:00 +0000

    * Update h2's du-fixes stub. It now identifies sgfxi as a "caller" and   downloads/executes sgfxi immediately. * Add symlink /usr/sbin/sgfxi -> /usr/sbin/du-fixes-h2.sh to actiavte above   change. * Update debian/copyright to reflect Harold Hope's and I influence. * Add myself to Uploaders.

--- du-fixes-h2.sh
+++ du-fixes-h2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-## note: save file as du-fixes-h2.sh
-
 PREFIX="/usr/local/bin/"
 UPSTREAM="http://techpatterns.com/downloads/distro/"
-DUF="du-fixes-h2.sh"
+
+# this will handle stub for any other script also
+CALLER=$(basename $0)
 
 error_handler()
 {
@@ -16,42 +16,56 @@ error_handler()
 			message='Failed to download script!'
 			;;
 		3)
-			message="${PREFIX}${DUF} not executable!"
+			message="${PREFIX}${SCRIPT} not executable!"
 			;;
 		4)
 			message='Script download exited with errors.'
+			;;
+		5)
+			message='Unknown script requested.'
 			;;
 		*)
 			message='Unknown error, exiting now.'
 			;;
 	esac
 	echo "ERROR: $message - Error No: $1"
-	
+
 	exit $1
 }
 
-# user login test cannot run first because it kills -h
+# set the SCRIPT value
+case $CALLER in
+	sgfxi)
+		SCRIPT="sgfxi"
+		;;
+	du-fixes-h2.sh)
+		SCRIPT="du-fixes-h2.sh"
+		;;
+	*)
+		error_handler 5
+		;;
+esac
 
-# if it exists, du-fixes will handle the root tests. Root tests cannot
+# if $SCRIPT exists, it will handle the root tests. Root tests cannot
 # be run first because they kill the -h help function
-if [ -x "${PREFIX}${DUF}" ]; then
-	exec "${PREFIX}${DUF}" "$@"
+if [ -x "${PREFIX}${SCRIPT}" ]; then
+	exec "${PREFIX}${SCRIPT}" "$@"
 else
 	# only in case of requirement to download script do root test, and just exit
 	# if not root, anything else is counterintuitive I think for users.
 	[ "$(id -u)" -ne 0 ] && error_handler 1
 
-	if [ ! -f "${PREFIX}${DUF}" ]; then
-		wget -Nc -O"${PREFIX}${DUF}" "${UPSTREAM}${DUF}" || error_handler 4
+	if [ ! -f "${PREFIX}${SCRIPT}" ]; then
+		wget -Nc -O"${PREFIX}${SCRIPT}" "${UPSTREAM}${SCRIPT}" || error_handler 4
 	fi
-	[ -f "${PREFIX}${DUF}" ]   || error_handler 2
 	
-	[ -x "${PREFIX}${DUF}" ] || chmod +x "${PREFIX}${DUF}"
-	
-	if [ -x "${PREFIX}${DUF}" ]; then
-		exec "${PREFIX}${DUF}" "$@"
+	[ -f "${PREFIX}${SCRIPT}" ] || error_handler 2
+   
+	[ -x "${PREFIX}${SCRIPT}" ] || chmod +x "${PREFIX}${SCRIPT}"
+
+	if [ -x "${PREFIX}${SCRIPT}" ]; then
+		exec "${PREFIX}${SCRIPT}" "$@"
 	else
 		error_handler 3
 	fi
 fi
-
